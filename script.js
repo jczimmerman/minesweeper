0a611b0809458a7573512d4ae03d1f9128e8f6d5
Justice Lee <txawjcojlee@gmail.com>
2019-12-03 16:03:25 -0500

    made the tiles clickable

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ for (row = 0; row < 9; row++) {
     })
     let cell = document.createElement('td');
     tableRow.appendChild(cell);
+    cell.addEventListener("click", tileReveal)
   }
   document.querySelector('table').appendChild(tableRow);
+}
+
+function tileReveal(event) {
+  console.log("testeetatsetse");
 }
