4d3bfb48c09affa9242b9992a9929e9cff5bea5e
Brendan Horst <yoshistar2000@gmail.com>
2019-12-03 16:07:53 -0500

    added a check for if cell 0, 0 is a bomb upon click
    if 0, 0 is a bomb, all the cells become unclickable

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ for (row = 0; row < 9; row++) {
   document.querySelector('table').appendChild(tableRow);
 }
 
+
 function tileReveal(event) {
   console.log("testeetatsetse");
+  if (grid[0][0].isBomb === true) {
+    console.log(`You hit a bomb at cell 0, 0`);
+    for (let row = 0; row < 9; row++) {
+      for (let column = 0; column < 9; column++) {
+        document.querySelectorAll('tr')[row].childNodes[column].removeEventListener('click', tileReveal);
+      }
+    }
+  }
 }
