c196cce3e40dafa26158ad87d1f266f7dbf814b6
Brendan Horst <yoshistar2000@gmail.com>
2019-12-04 13:02:17 -0500

    put the code for drawing the grid into a function

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let grid = [];
 
-for (row = 0; row < 9; row++) {
-  let tableRow = document.createElement('tr');
-  grid.push([]);
-  for (column = 0; column < 9; column++) {
-    grid[row].push({
-      isFlagged: false,
-      isBomb: false
-    })
-    let cell = document.createElement('td');
-    tableRow.appendChild(cell);
-    cell.addEventListener("click", tileReveal)
+const drawGrid = () => {
+  for (row = 0; row < 9; row++) {
+    let tableRow = document.createElement('tr');
+    grid.push([]);
+    for (column = 0; column < 9; column++) {
+      grid[row].push({
+        isFlagged: false,
+        isBomb: false
+      })
+      let cell = document.createElement('td');
+      tableRow.appendChild(cell);
+      cell.addEventListener("click", tileReveal)
+    }
+    document.querySelector('table').appendChild(tableRow);
   }
-  document.querySelector('table').appendChild(tableRow);
 }
 
 
-function tileReveal(event) {
+const tileReveal = (event) => {
   console.log("testeetatsetse");
   if (grid[0][0].isBomb === true) {
     console.log(`You hit a bomb at cell 0, 0`);
@@ -27,3 +29,5 @@ function tileReveal(event) {
     }
   }
 }
+
+drawGrid();
