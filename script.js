aa8325144d1f6d497cec8b250ed3cb53b710b2a2
Brendan Horst <yoshistar2000@gmail.com>
2019-12-04 13:33:23 -0500

    added elementToGrid function
    it takes a element determined by event.target, and converts it to
    the corresponding location in the grid array

--- script.js
+++ script.js
@@ -17,14 +17,26 @@ const drawGrid = () => {
   }
 }
 
+const elementToGrid = (element) => {
+  let gridColumn;
+  let gridRow;
+  for (let i = 0; i < 9; i++){
+    if (element.parentNode.children[i] === element) gridColumn = i;
+  }
+  for (let i = 0; i < 9; i++){
+    if (document.querySelector('table').children[i] === element.parentNode) gridRow = i;
+  }
+  return grid[gridRow][gridColumn];
+}
 
 const tileReveal = (event) => {
+  let gridObject = elementToGrid(event.target);
   console.log("testeetatsetse");
-  if (grid[0][0].isBomb === true) {
-    console.log(`You hit a bomb at cell 0, 0`);
+  if (gridObject.isBomb === true) {
+    console.log(`You hit a bomb!`);
     for (let row = 0; row < 9; row++) {
       for (let column = 0; column < 9; column++) {
-        document.querySelectorAll('tr')[row].childNodes[column].removeEventListener('click', tileReveal);
+        document.querySelectorAll('tr')[row].children[column].removeEventListener('click', tileReveal);
       }
     }
   }
